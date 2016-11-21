0c10efbd3a8bb6b7e38528fd0fe8d49fe0c515fd
Osa__PL <osa12pl@gmail.com>
2016-11-21 17:54:31 +0100

    small change, to make sure gamedvr is not working

--- install_wim_tweak/cleanWin10.bat
+++ install_wim_tweak/cleanWin10.bat
@@ -80,6 +80,9 @@ IF ERRORLEVEL 1 (
 	sc delete XblAuthManager
 	sc delete XblGameSave
 	sc delete XboxNetApiSvc
+	:: Just to be 100% sure
+	reg add "HKEY_CURRENT_USER\System\GameConfigStore" /v GameDVR_Enabled /t REG_DWORD /d 0 /f
+	reg add "HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\GameDVR" /v AppCaptureEnabled /t REG_DWORD /d 0 /f
 )
 IF ERRORLEVEL 2 (
 	echo "Not deleting the Xbox app"
