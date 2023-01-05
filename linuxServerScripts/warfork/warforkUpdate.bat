68bb0bf841fbb9b681ec8c84aa5c7b76fff536c4
Michał Osowski <osa12pl@gmail.com>
2023-01-05 22:07:15 +0100

    Update warforkUpdate.bat

--- linuxServerScripts/warfork/warforkUpdate.bat
+++ linuxServerScripts/warfork/warforkUpdate.bat
@@ -1,3 +1,3 @@
 sudo systemctl stop warforkServer
-steamcmd  +@NoPromptForPassword 1 +login osa12pl Osa144mich144 +app_update 671610 validate +quit
+steamcmd  +@NoPromptForPassword 1 +login <user> <pass> +app_update 671610 validate +quit
 sudo systemctl start warforkServer
